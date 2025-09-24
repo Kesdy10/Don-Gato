3fdbf51cca3628470fbb31ca70de18d701281f88
Kesdy10 <106620715+Kesdy10@users.noreply.github.com>
2025-09-23 19:48:03 -0600

    Add files via upload

--- Proyecto/js/script.js
+++ Proyecto/js/script.js
@@ -6,3 +6,20 @@ function togglePassword() {
         input.type = "password";
     }
 }
+
+
+const botones = document.querySelectorAll(".datos-producto button");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        botones.forEach(b => {
+            b.classList.remove("activo");
+            b.style.backgroundColor = "";
+            b.style.color = "";
+        });
+
+        boton.classList.add("activo");
+        boton.style.backgroundColor = "yellowgreen";
+        boton.style.color = "white";
+    });
+});
